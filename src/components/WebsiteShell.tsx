2621054ed99045379f52cb18cef8d0e1daa61a2c
Doruk Eray <doruk@dorkodu.com>
2023-10-08 09:32:29 +0300

    WIP: WebsiteShell, Layout, Index, VanillaExtract

---
+++ src/components/WebsiteShell.tsx
@@ -0,0 +1,11 @@
+import { Box } from "@mantine/core";
+
+export default function WebsiteShell() {
+  return (
+    <Box>
+      <div></div>
+      <div></div>
+      <div></div>
+    </Box>
+  );
+}
