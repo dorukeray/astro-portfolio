65cdc4c576fe8dd3e4c4850a04d9b0d027e09634
Doruk Eray <doruk@dorkodu.com>
2023-10-08 09:31:39 +0300

    add integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
 
+import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), react()]
+  integrations: [mdx(), react(), sitemap()],
 });
